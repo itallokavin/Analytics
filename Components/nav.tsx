09c0af479133f058488f6a8205464e5a9c8cb29a
Itallo Kavin <italo.keven50@gmai.com>
2022-12-08 17:20:29 -0300

    add icons in menu

--- Components/nav.tsx
+++ Components/nav.tsx
@@ -2,6 +2,10 @@ import style from '../styles/Navbar.module.css'
 import Link from 'next/link'
 import Image from 'next/image'
 import Logo from '../assets/images/logo.png'
+import { faHouse } from '@fortawesome/free-solid-svg-icons'
+import { faClock} from '@fortawesome/free-solid-svg-icons'
+import { faAngleRight} from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 export default function Navbar() {
     return (
@@ -16,8 +20,34 @@ export default function Navbar() {
           />
         </div>
         <ul className={style.list}>
-            <li className={style.categoryHome}><Link href="/">Home</Link></li>        
-            <li className={style.category}><Link href="/logs">Logs</Link></li>
+            <li className={style.category}>
+              <div className={style.campIcon}>
+                <FontAwesomeIcon 
+                  icon={faHouse}
+                  className={style.icon}
+                />
+              </div>
+              <div className={style.campCategory}>
+                <Link href="/">Home</Link>
+              </div>
+            </li>        
+            <li className={style.category}>
+              <div className={style.campIcon}>
+                <FontAwesomeIcon 
+                  icon={faClock}
+                  className={style.icon}
+                />
+              </div>
+              <div className={style.campCategory}>
+                <Link href="/logs">Logs</Link>
+              </div>
+              <div className={style.campArrow}>
+                <FontAwesomeIcon 
+                  icon={faAngleRight}
+                  className={style.iconArrow}
+                />
+              </div>
+            </li>
         </ul>
       </div>
     )
